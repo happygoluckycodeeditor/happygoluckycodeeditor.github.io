41881fe29d01225111cc6d208280f4b3d8ad662e
TanmayB <tanmay.bagwe.tb@gmail.com>
2023-03-19 00:20:27 +0100

    Added right scroll

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* Adding right side Javascript Scroll*/
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show1');
+        } 
+    });
+});
+
+const hiddenElements1 = document.querySelectorAll('.hidden1');
+hiddenElements1.forEach((el) => observer1.observe(el));
