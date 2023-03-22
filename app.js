7dfaeaf142758429b4e15905e0964a05b833e8d1
TanmayB <tanmay.bagwe.tb@gmail.com>
2023-03-22 22:45:39 +0100

    Adding changing colors on scroll

--- app.js
+++ app.js
@@ -24,3 +24,23 @@ const hiddenElements1 = document.querySelectorAll('.hidden1');
 hiddenElements1.forEach((el) => observer1.observe(el));
 
 /* The Javascript is currently dormant at the moment */
+
+/* Change color with scroll */
+const colors = ['', 'lightPink', 'pastelBlue']
+
+const sections = [...document.getElementsByTagName('section')]
+
+window.addEventListener('scroll', function () {
+
+  const scrollFromTop = window.pageYOffset
+
+  for (let i = 0; sections.length > i; i++) {
+
+    if (scrollFromTop <= sections[i].offsetTop) {
+      document.body.className = colors[i] 
+      break
+    } 
+
+  }
+
+})
