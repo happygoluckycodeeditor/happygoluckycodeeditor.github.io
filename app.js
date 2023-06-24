c53a5c90935c19c1488ec4884560be57343ffa52
TanmayB <tanmay.bagwe.tb@gmail.com>
2023-06-24 23:09:11 +0200

    Added Alert Notif for my figma app

--- app.js
+++ app.js
@@ -60,3 +60,16 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+/*Alert Messages */
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
